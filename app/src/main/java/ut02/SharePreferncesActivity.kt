dd5c76e34dbf3bfb3009a1b0a4a33d3e284f90e0
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-10-07 19:50:39 +0200

    Creada nueva activity SharePreferes

---
+++ app/src/main/java/ut02/SharePreferncesActivity.kt
@@ -0,0 +1,11 @@
+package ut02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharePreferncesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_prefernces)
+    }
+}
