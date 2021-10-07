75a6ae629dca7609855a11b305e34c58d9d97fd0
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-10-07 19:54:17 +0200

    Cambio de nombre al SharePreferencesActivity por erro al escribirlo

--- app/src/main/java/ut02/SharePreferencesActivity.kt
+++ app/src/main/java/ut02/SharePreferencesActivity.kt
@@ -2,8 +2,9 @@ package ut02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tijanidian.add_playground.R
 
-class SharePreferncesActivity : AppCompatActivity() {
+class SharePreferencesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share_prefernces)
