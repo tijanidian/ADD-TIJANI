506cef01f1472da1c8de19a5302885ddf64ab595
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-10-28 20:31:26 +0200

    Clase para guardar las tapas

---
+++ app/src/main/java/ut02/exercise02/TapaRepository.kt
@@ -0,0 +1,18 @@
+package ut02.exercise02
+
+class TapaRepository(private val localStorage: LocalStorage<TapaLocalModel>) {
+
+    /**
+     * Solo guarda la tapa, no le importa donde se guarde. Almacena una plantilla para alacenar datos
+     */
+    fun save(tapa:TapaLocalModel){
+        localStorage.save(tapa)
+    }
+
+    /**
+     *
+     */
+    fun fetch(id:Int):TapaLocalModel?{
+        return localStorage.fetch(id.toString())
+    }
+}
