f9d8395a80e501305019099c5a57bb28a8dc75ca
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-09 19:11:17 +0100

    Recuperamos datos locales/ remotos

---
+++ app/src/main/java/ut03/ex02/data/PersonDataRepository.kt
@@ -0,0 +1,13 @@
+package ut03.ex02.data
+
+import ut03.ex02.domain.PersonModel
+import ut03.ex02.domain.PersonRepository
+
+
+//Su objetivo es implementar la interfaz que hemos creado en domain
+class PersonDataRepository:PersonRepository{
+    override fun savePerson(personModel: PersonModel) {
+        TODO("Not yet implemented")
+    }
+
+}
