69b6c10e94053b1fe9a734fe89d0a0e3176ce945
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-16 20:33:48 +0100

    Implementados casos de usos

--- app/src/main/java/ut03/ex03/presentation/Ut03Ex03Activity.kt
+++ app/src/main/java/ut03/ex03/presentation/Ut03Ex03Activity.kt
@@ -1,21 +1,35 @@
 package ut03.ex03.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.tijanidian.add_playground.R
+import ut03.ex03.app.RetrofitApiClient
+import ut03.ex03.data.AlertDataRepository
+import ut03.ex03.data.remote.AlertsRemoteSource
+
+import ut03.ex03.domain.FindAlertUseCase
+import ut03.ex03.domain.GetAlertsUseCase
 
 
 class Ut03Ex03Activity : AppCompatActivity() {
-    private val viewModel:Ut03Ex03ViewModel by viewModels()
-    override  fun onCreate(savedInstanceState: Bundle?) {
+    private val viewModel: Ut03Ex03ViewModel = Ut03Ex03ViewModel(GetAlertsUseCase(AlertDataRepository(AlertsRemoteSource(RetrofitApiClient()))),
+        FindAlertUseCase(AlertDataRepository(AlertsRemoteSource(RetrofitApiClient())))
+        )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ut03_ex2)
+        executeQueryById("2")
         executeQuery()
     }
 
 
-    fun executeQuery(){
+    fun executeQueryById(alertId: String) {
+        viewModel.getAlert(alertId)
+    }
+
+    fun executeQuery() {
         viewModel.getAlerts()
     }
 }
