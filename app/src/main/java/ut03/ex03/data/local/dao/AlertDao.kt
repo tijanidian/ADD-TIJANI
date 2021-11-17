5ba41b1a939f84884eaa217ef76e671055314b0e
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-17 16:05:04 +0100

    Añadido el inser para la bd

--- app/src/main/java/ut03/ex03/data/local/dao/AlertDao.kt
+++ app/src/main/java/ut03/ex03/data/local/dao/AlertDao.kt
@@ -1,6 +1,7 @@
 package ut03.ex03.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
 import ut03.ex03.data.local.entity.AlertEntity
@@ -13,4 +14,8 @@ interface AlertDao {
 
     @Query("SELECT * FROM alerts WHERE alertId = :alert_id")
     fun findById(alert_id:String):AlertEntity
+
+    @Insert
+    fun insertAlerts(alertEntity: AlertEntity)
+
 }
