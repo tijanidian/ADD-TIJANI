443e9952bfe793e247bfbc5b59994769e8041a86
Tijani <73607974+tijanidian@users.noreply.github.com>
2021-11-17 16:05:38 +0100

    añadida funcion initDb para hacer la logica de cacheo de datos

--- app/src/main/java/ut03/ex03/presentation/Ut03Ex03Activity.kt
+++ app/src/main/java/ut03/ex03/presentation/Ut03Ex03Activity.kt
@@ -1,5 +1,6 @@
 package ut03.ex03.presentation
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +9,10 @@ import com.tijanidian.add_playground.R
 import ut03.ex03.app.RetrofitApiClient
 import ut03.ex03.app.Ut03Ex03DataBase
 import ut03.ex03.data.AlertDataRepository
+import ut03.ex03.data.local.LocalDataSource
+import ut03.ex03.data.local.entity.AlertEntity
 import ut03.ex03.data.remote.AlertsRemoteSource
+import ut03.ex03.domain.AlertModel
 import ut03.ex03.domain.FindAlertUseCase
 import ut03.ex03.domain.GetAlertsUseCase
 
@@ -30,21 +34,36 @@ class Ut03Ex03Activity : AppCompatActivity() {
 
 
     private fun initDb() {
-        val apiClient=RetrofitApiClient()
         val db=Room.databaseBuilder(applicationContext,Ut03Ex03DataBase::class.java,"alert-db"
         ).build()
 
         Thread(Runnable {
-            var alerts=db.alertDao().findAll()
-            if(alerts==null){
-                val alertRemote=apiClient.getALerts()
-                Log.d("@tijani1","$alertRemote")
+            var alerts=db.alertDao().findById("1")
+
+            if(alerts == null){
+                db.alertDao().insertAlerts(AlertEntity("1","titulo1","summary1",1,"data","body","source"))
+                alerts=db.alertDao().findById("1")
+
             }else{
-                Log.d("@tijani2","$alerts")
+             initSharedPref()
             }
         }).start()
     }
 
+    private fun initSharedPref(){
+        val apiClient=RetrofitApiClient()
+        val alerts=apiClient.getALerts()
+        Log.d("@tijani","$alerts")
+
+        val localDataSource=LocalDataSource(this)
+        localDataSource.saveAsync("tijani_shared","$alerts")
+        val data=localDataSource.readEncrypt("tijani_shared")
+        Log.d(TAG, data!!)
+        data?.run {
+            Log.d(TAG, this)
+        }
+
+    }
 
 
 
--- app/src/main/java/ut03/ex03/presentation/Ut03Ex03ViewModel.kt
+++ app/src/main/java/ut03/ex03/presentation/Ut03Ex03ViewModel.kt
@@ -2,24 +2,40 @@ package ut03.ex03.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import ut03.ex03.app.RetrofitApiClient
 import ut03.ex03.domain.FindAlertUseCase
+
 import ut03.ex03.domain.GetAlertsUseCase
 
-class Ut03Ex03ViewModel(private val getAlertsUseCase: GetAlertsUseCase, private val findAlertUseCase: FindAlertUseCase): ViewModel() {
+class Ut03Ex03ViewModel(
+    private val getAlertsUseCase: GetAlertsUseCase,
+    private val findAlertUseCase: FindAlertUseCase
+) : ViewModel() {
 
-    fun getAlerts(){
-        val apiClient=RetrofitApiClient()
-        viewModelScope.launch { Dispatchers.Main
+    fun getAlerts() {
+        val apiClient = RetrofitApiClient()
+        Thread(Runnable {
             getAlertsUseCase.execute()
-            val users=apiClient.getALerts()
-            Log.d("@tijani","$users")
-        }
+            val users = apiClient.getALerts()
+            Log.d("@tijani", "$users")
+        }).start()
+
+
+    }
+    fun getAlert(alertId: String) {
+        val apiClient = RetrofitApiClient()
+
+        Thread(Runnable {
+            findAlertUseCase.execute(alertId)
+            val user = apiClient.getAlert(alertId)
+
+            Log.d("@tijani,", "$user")
+        }).start()
 
     }
 
-     fun getAlert(alertId:String)=findAlertUseCase.execute(alertId)
 }
+
+
+
+
